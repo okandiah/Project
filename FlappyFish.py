cd9a244254b0d93e84f717d4fe3cbccd1d37d11c
okandiah <oviakandiah@gmail.com>
2023-11-06 17:18:09 -0500

    creating screen

--- FlappyFish.py
+++ FlappyFish.py
@@ -2,6 +2,13 @@ import pygame
 from pygame.locals import *
 import random
 
+pygame.init() 
+
+width_screen = 1024
+height_screen = 768
+
+screen = pygame.display.set_mode(width_screen, height_screen)
+
 #Main/ Starting Screen code
     #Button to show rules, button to start game
 
