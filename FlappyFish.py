5fd04316263539c7943f5604193271a23f7138f3
okandiah <oviakandiah@gmail.com>
2023-11-06 16:53:38 -0500

    setup

---
+++ FlappyFish.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+import random
+
+#Main/ Starting Screen code
+    #Button to show rules, button to start game
+
+#Moving game screen
+
+#Game animations and visuals
+
+#Controls of the bird
+
+#Adding the obstacles
+
+#Adding a score (counter) 
+
+#Replay game after loss
