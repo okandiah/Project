d5f99ea0210492cad935e484c8b6c564291ed3d2
anoushka1111 <saha.anoushka@gmail.com>
2023-11-08 10:24:54 -0500

    mainscreen1

--- FlappyFish.py
+++ FlappyFish.py
@@ -8,6 +8,18 @@ width_screen = 1024
 height_screen = 768
 
 screen = pygame.display.set_mode(width_screen, height_screen)
+pygame.caption.set_caption("Main Menu")
+
+def create_main_text(text,font,text_colour,x,y):
+    img = font.render(text, True, text_colour)
+    screen.blit(img, (x, y))
+
+run = True
+
+while run:
+    screen.fill((52, 78, 91))
+
+
 
 #Main/ Starting Screen code
     #Button to show rules, button to start game
