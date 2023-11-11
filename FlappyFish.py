16377f931c5282f64ae9a3c8f5bc95cd7ef260fa
anoushka1111 <saha.anoushka@gmail.com>
2023-11-11 14:22:22 -0500

    playscreen

--- FlappyFish.py
+++ FlappyFish.py
@@ -2,27 +2,50 @@ import pygame
 from pygame.locals import *
 import random
 import sys
+
 pygame.init()
 
-timer=pygame.time.Clock()
-fps=60
-    # screen size
+timer = pygame.time.Clock()
+fps = 60
+
+# screen size
 size = (1024, 768)
 width = 1024
 height = 768
 
 background = pygame.image.load('gamebackground.png')
-    #Main Screen
-    # opens up a window
+
+# opens up a window
 screen = pygame.display.set_mode(size)
-screen.blit(background, (0,0))
+screen.blit(background, (0, 0))
+
+
+def play_screen():
+    run = True
+    background2 = pygame.image.load('gamebackground.png')
+    background_scroll = 0
+    scroll_speed = 4
+
+    while run:
+        timer.tick(fps)
+        screen.blit(background, (0, 0))
+        screen.blit(background2, (background_scroll, 768))
+        background_scroll -= scroll_speed
+
+        if abs(background_scroll) > 35:
+            background_scroll = 0
+
+        # for loop allows the user to quit the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
 
 def main_screen():
-    #Main Screen
-    # opens up a window
-    screen.blit(background, (0,0))
-
-# white color
+    # white color
     color = (0, 0, 0)
 
     # light shade of the button
@@ -34,15 +57,15 @@ def main_screen():
     # defining a font
     smallfont = pygame.font.SysFont('Corbel', 35)
 
-# rendering a text written in
-# this font
+    # rendering a text written in
+    # this font
     text = smallfont.render('PLAY', True, color)
 
     while True:
 
         for event in pygame.event.get():
 
-            if type(event)== pygame.QUIT:
+            if type(event) == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
@@ -50,30 +73,15 @@ def main_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
 
                 # if the mouse is clicked on the
-                # button the game is terminated
+                # button, switch to the play screen
                 mouse = pygame.mouse.get_pos()
                 button_width = 200
                 button_height = 70
                 button_x = (width - button_width) // 2
                 button_y = (height - button_height) // 2
-                #play game
+
                 if button_x <= mouse[0] <= button_x + button_width and button_y <= mouse[1] <= button_y + button_height:
-                    run = True
-                    background2=pygame.image.load('gamebackground.png')
-                    background_scroll=0
-                    scroll_speed=4
-                    while run:
-                        timer.tick(fps)
-                        screen.blit(background,(0,0))
-                        screen.blit(background2,(background_scroll,768))
-                        background_scroll -= scroll_speed
-                        if abs(background_scroll)>35:
-                            background_scroll=0
-                    #for loop allows user to quit game
-                        for event in pygame.event.get():
-                            if type(event)== pygame.QUIT:
-                                run=False
-                    pygame.display.update
+                    play_screen()
                 else:
                     sys.exit()
 
@@ -91,13 +99,13 @@ def main_screen():
         mouse = pygame.mouse.get_pos()
 
         # if mouse is hovered on a button it
-        # changes to lighter shade
+        # changes to a lighter shade
         if button_x <= mouse[0] <= button_x + button_width and button_y <= mouse[1] <= button_y + button_height:
             pygame.draw.rect(screen, color_light, [button_x, button_y, button_width, button_height])
         else:
             pygame.draw.rect(screen, color_dark, [button_x, button_y, button_width, button_height])
 
-    # superimposing the text onto our button
+        # superimposing the text onto our button
         screen.blit(text, (button_x + 65, button_y + 20))
 
         # updates the frames of the game
@@ -105,6 +113,7 @@ def main_screen():
 
 
 main_screen()
+
 #moving_game_screen()
 
 #Main/ Starting Screen code
